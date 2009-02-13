552d96e0294f0d7dd49eb8e906ca01ee28d95e41
Garret Alfert <alfert@wevelop.de>
2009-02-13 01:04:07 +0100

    install everythin withou asking

--- base.rb
+++ base.rb
@@ -6,6 +6,7 @@ run "rm public/index.html"
 run "rm public/favicon.ico"
 run "rm public/robots.txt"
 run "rm public/images/rails.png"
+run "rm -f public/javascripts/*"
 
 # Initialize git repository
 git :init
@@ -15,33 +16,18 @@ run "cp config/database.yml config/database.yml.example"
  
 # Add some common gems
 gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
-
-if yes?("Do you want to use Google Analytics?")
-  gem 'rubaidh-google_analytics', 
-    :lib => 'rubaidh/google_analytics', 
-    :source => 'http://gems.github.com'
-  initializer 'google_analytics.rb', "Rubaidh::GoogleAnalytics.tracker_id = ''"
-  puts "Add your tracker id to 'config/initializers/google_analytics.rb'"
-end
-
-#gem 'RedCloth', :lib => 'redcloth', :version => '~> 3.0.4'
-#gem 'mocha'
+gem 'rubaidh-google_analytics', :lib => 'rubaidh/google_analytics', :source => 'http://gems.github.com'
+initializer 'google_analytics.rb', "Rubaidh::GoogleAnalytics.tracker_id = ''"
 
 # Add plugins
 plugin 'jrails', :git => 'git://github.com/aaronchi/jrails.git', :submodule => true
 plugin 'asset_packager', :git => 'git://github.com/sbecker/asset_packager.git', :submodule => true
-
-if yes?("Do you want to use Hoptoad?")
-  plugin 'hoptoad_notifier', :git => 'git://github.com/thoughtbot/hoptoad_notifier.git', :submodule => true
-  initializer 'hoptoad.rb', <<-END
+plugin 'hoptoad_notifier', :git => 'git://github.com/thoughtbot/hoptoad_notifier.git', :submodule => true
+initializer 'hoptoad.rb', <<-END
 HoptoadNotifier.configure do |config|
   config.api_key = ''
 end
-  END
-  puts "Remove from the ApplicationController: 'include ExceptionNotifiable'"
-  puts "Add to the ApplicationController: 'include HoptoadNotifier::Catcher'"
-  puts "Add your API key to 'config/initializers/hoptoad.rb'"
-end
+END
 
 # Set up plugins, migrations, etc.
 rake 'jrails:install:javascripts'
@@ -62,3 +48,9 @@ git :submodule => "init"
 # Commit everything
 git :add => "."
 git :commit => "-a -m 'Initial commit'"
+
+puts "Success"
+puts "Now do what?"
+puts "Add your Google Analytics tracker id to 'config/initializers/google_analytics.rb'"
+puts "Replace the line 'include ExceptionNotifiable' with 'include HoptoadNotifier::Catcher' in the ApplicationController"
+puts "Add your Hoptoad API key to 'config/initializers/hoptoad.rb'"
