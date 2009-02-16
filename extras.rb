f6093896f591e1cf37dd06081f763ad10f962f87
Garret Alfert <alfert@wevelop.de>
2009-02-17 00:03:34 +0100

    Add template for extra packages

---
+++ extras.rb
@@ -0,0 +1,17 @@
+load_template "http://github.com/galfert/rails-templates/raw/master/base.rb"
+
+plugin 'open_id_authentication', :git => 'git://github.com/rails/open_id_authentication', :submodule => true
+plugin 'restful-authentication', :git => 'git://github.com/technoweenie/restful-authentication', :submodule => true
+
+gem 'thoughtbot-paperclip', :lib => 'paperclip', :source => 'http://gems.github.com'
+
+rake("gems:install", :sudo => true)
+
+generate("authenticated", "user session")
+
+# Initialize submodules
+git :submodule => "init"
+
+# Commit everything
+git :add => "."
+git :commit => "-a -m 'Add extra packages'"
